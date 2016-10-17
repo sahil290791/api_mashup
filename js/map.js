5171f87c2c0d0d898c452fdaa133e75c3b919641
Sahil <sahil.prajapati@tcs.com>
2016-10-17 10:21:15 +0530

    working with webpack

---
+++ js/map.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+	var map;
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+	});
+	}
+	initMap();
+};
